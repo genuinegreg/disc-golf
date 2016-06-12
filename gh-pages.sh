108e373148f44c09a4b17405a6844183af2b31d9
Grégoire Audoux <projects@gregoire-audoux.fr>
2016-06-12 16:34:34 +0200

    small fix

--- gh-pages.sh
+++ gh-pages.sh
@@ -2,12 +2,12 @@
 
 
 git checkout gh-pages
-git rebase master
+git reset --hard master
 
 jspm bundle main.js build.js
 git add -f jspm_packages/system.js build.js build.js.map
 
 git commit -m "gh-pages"
-git push origin gh-pages
+git push origin gh-pages -f
 
 git checkout master
